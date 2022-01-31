eee0aab293c9704cc1029928e3fdf6478c196236
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2022-01-31 22:44:14 +0530

    day 10

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:catalog/pages/widgets/drawers.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -16,7 +17,66 @@ class HomePage extends StatelessWidget {
           child: Text('stating from beginning $days days'),
         ),
       ),
-      drawer: MyDrawer(),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.deepPurple,
+          child: ListView(
+            children: [
+              DrawerHeader(
+                padding: EdgeInsets.zero,
+                child: UserAccountsDrawerHeader(
+                  margin: EdgeInsets.zero,
+                  accountName: Text(
+                    'Arti Chaudhary',
+                    textScaleFactor: 1.3,
+                  ),
+                  accountEmail: Text(
+                    'chaudhary.aarti@gmail.com',
+                    textScaleFactor: 1.3,
+                  ),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage:
+                        AssetImage('assets/images/artichaudhary.png'),
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  CupertinoIcons.home,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Home',
+                  textScaleFactor: 1.3,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  CupertinoIcons.profile_circled,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Profile',
+                  textScaleFactor: 1.3,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  CupertinoIcons.at,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Email Me',
+                  textScaleFactor: 1.3,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
